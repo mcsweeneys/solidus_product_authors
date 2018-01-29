fadfff363e0142984657e8c9dad8041e56b17d96
Brian Christian <brchristian@gmail.com>
2018-01-29 15:55:04 -0800

    update deface hash

--- app/overrides/add_authors_link_to_products_side_bar.rb
+++ app/overrides/add_authors_link_to_products_side_bar.rb
@@ -3,5 +3,5 @@ Deface::Override.new(
   virtual_path: 'spree/admin/shared/_product_tabs',
   insert_bottom: "[data-hook='admin_product_tabs']",
   partial: 'spree/admin/authors/products_tab',
-  original: '986f195c216143a965d4caf30906b9006d477964'
+  original: '836913aa2407a97efeb9a956b932d6474c6fcc8b'
 )
