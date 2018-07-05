8cad76be3f7f6c1ff675c01ea7f826f6e19cf0d0
Brian Christian <brchristian@gmail.com>
2018-07-05 12:53:57 -0700

    don't install empty assets

--- lib/generators/spree_product_authors/install/install_generator.rb
+++ lib/generators/spree_product_authors/install/install_generator.rb
@@ -1,15 +1,7 @@
 module SpreeProductAuthors
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      def add_javascripts
-        append_file 'app/assets/javascripts/store/all.js', "//= require store/spree_product_authors\n"
-        append_file 'app/assets/javascripts/admin/all.js', "//= require admin/spree_product_authors\n"
-      end
 
-      def add_stylesheets
-        inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_product_authors\n", before: /\*\//, verbose: true
-        inject_into_file 'app/assets/stylesheets/admin/all.css', " *= require admin/spree_product_authors\n", before: /\*\//, verbose: true
-      end
 
       def add_migrations
         run 'bundle exec rake railties:install:migrations FROM=spree_product_authors'
